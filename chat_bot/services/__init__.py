2a7b085256d71e47bf3ac111197ec609312ab1fe
KarinaKhinevich <karina.khinevich@gmail.com>
2025-09-19 20:28:13 +0200

    Add chat service

--- chat_bot/services/__init__.py
+++ chat_bot/services/__init__.py
@@ -2,6 +2,7 @@
 
 from .document_service import DocumentService
 from .openai_service import summarize_document
+from .openai_service.chat_service import ChatService
 from .pg_document_service import PGDocumentService
 
-__all__ = ["DocumentService", "summarize_document", "PGDocumentService"]
+__all__ = ["DocumentService", "summarize_document", "PGDocumentService", "ChatService"]
